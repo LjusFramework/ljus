a3f42c4a63df40654ddbda3329cc52a3e502461b
Erik Partridge <erik@centsforchange.net>
2017-09-09 20:57:09 -0400

    Update Request.h

--- ljus/http/utils/Request.h
+++ ljus/http/utils/Request.h
@@ -5,20 +5,15 @@
 #ifndef LJUS_REQUEST_H
 #define LJUS_REQUEST_H
 
-#include <pistache/http.h>
 #include "encryption/Crypt.h"
 
-using namespace Pistache;
 namespace Ljus {
     class Request {
 
     public:
-        explicit Request( Pistache::Http::Request request );
-
         std::string method;
         std::string body;
         std::string resource;
-        Http::Header::Collection headers;
 
     };
 }
