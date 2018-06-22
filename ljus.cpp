b02a8273583448626f61d02b4a2e4b56d268727b
Erik A. Partridge <erik.partridge@mail.mcgill.ca>
2018-06-22 15:12:32 -0400

    Updating Travis

--- ljus.cpp
+++ ljus.cpp
@@ -1,8 +1,20 @@
 #include "ljus.h"
+#include "./external/http/http.h"
 
+using namespace http;
+
+#include <stdio.h>
 
 int main() {
 
     //Pass to routes function
+    Server server([](auto &req, auto &res) {
+        for (auto elem : req.headers) {
+
+            std::cout << elem.second << "\n";
+        }
+    });
+
+    server.listen("0.0.0.0", 8000);
     return 0;
 }
