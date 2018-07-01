01d79f9fcc752cd5aaed36b531c97b19a01660fa
Erik A. Partridge <erik.partridge@mail.mcgill.ca>
2018-06-30 20:38:51 -0400

    Test complete, removing HTTP which was an issue

--- ljus/cookie/CookieJar.h
+++ ljus/cookie/CookieJar.h
@@ -5,12 +5,9 @@
 #ifndef LJUS_COOKIEJAR_H
 #define LJUS_COOKIEJAR_H
 
-#include "http/http.h"
 #include <string>
 #include <queue>
 #include <unordered_map>
-
-using namespace http;
 
 struct Cookie {
     std::string name;
@@ -56,7 +53,7 @@ public:
      * @return
      */
     Cookie forever(std::string name, std::string value, std::string path = std::string(), std::string domain = std::string(), bool secure = false,
-            bool httpOnly = true, bool raw = false, string sameSite = std::string());
+            bool httpOnly = true, bool raw = false, std::string sameSite = std::string());
     /**
      *
      * @param name
@@ -87,7 +84,7 @@ public:
     Cookie dequeue(std::string name);
 
 private:
-    unordered_map<std::string, Cookie> cookieQueue;
+    std::unordered_map<std::string, Cookie> cookieQueue;
 
     std::string *getPathAndDomain(std::string path, std::string domain, bool secure, std::string sameSite);
 
